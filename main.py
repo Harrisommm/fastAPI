0b7e6a4c14be2ecb3fbf02976dc432faff9bc399
HarrisonKim <wootak95@gmail.com>
2023-05-22 19:04:00 +0900

    add delete post

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Optional
-from fastapi import Body, FastAPI
+from fastapi import Body, FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
@@ -10,15 +11,35 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
+my_posts = [{"title:": "title", "content": "content","id": 1}]
+
+def find_post(id):
+    for p in my_post:
+        if ["id"] == id:
+            return p
 @app.get("/")
 def get_user():
     return {"message": "Hello World"}
 
 @app.get("/posts")
 def get_posts():
-    return {"message": "Hello World"}
+    return {"data": my_posts}
 
-@app.post("/createposts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
-    print(post.dict())
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0,10000)
+    my_posts.append(post.dic())
+    return {"data": post_dict}
+
+@app.get("/posts/{id}")
+def get_posts(id: int):
+
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
     return {"data": post}
+
+@app.delete("/posts/{id}")
+def delete_post():
+    return {"data": my_posts}
