00685005212c8201f5558a5172271f5dcd6c040e
HarrisonKim <wootak95@gmail.com>
2023-05-28 13:09:58 +0900

    hash user password 	modified:   app/main.py 	modified:   app/models.py 	modified:   app/schemas.py 	new file:   app/tempCodeRunnerFile.py 	new file:   app/utils.py

--- app/main.py
+++ app/main.py
@@ -3,10 +3,9 @@ from typing import List
 import psycopg2 
 from psycopg2.extras import RealDictCursor
 import time
-from . import models, schemas
+from . import models, schemas, utils
 from .database import engine, get_db
 from sqlalchemy.orm import Session
-
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -66,3 +65,13 @@ def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends
     post_query.update(updated_post.dict(), synchronize_session=False)
     db.commit()
     return post_query.first()
+
+@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    hashed_password = utils.hash(user.password)
+    user.password = hashed_password 
+    new_user = models.Uost(**user.dict)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
