d0e7f43b8a72230ea20d88b2217752689699d85d
HarrisonKim <wootak95@gmail.com>
2023-05-31 18:05:19 +0900

    remove unused imports 	modified:   app/main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,4 @@
 from fastapi import FastAPI
-import psycopg2 
-from psycopg2.extras import RealDictCursor
-import time
 from . import models
 from .database import engine
 from .routers import post, user, auth
@@ -9,16 +6,6 @@ from .routers import post, user, auth
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-while True:
-    try:
-        conn = psycopg2.connect(host='localhost', database='fastAPI', user='wootak', password='password', port=5432, cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print("Database connected!")
-        break
-    except Exception as error:
-        print("Error: ", error)
-        time.sleep(4)
 
 my_posts = [{"title:": "title", "content": "content","id": 1}]
 
