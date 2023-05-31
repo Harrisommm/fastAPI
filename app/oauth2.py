317c05a1eb6651e2ca6910f51f81b47d464150a4
HarrisonKim <wootak95@gmail.com>
2023-05-31 18:32:41 +0900

    migrate sensitive infos to env 	new file:   app/config.py 	modified:   app/database.py 	modified:   app/main.py 	modified:   app/oauth2.py

--- app/oauth2.py
+++ app/oauth2.py
@@ -4,17 +4,18 @@ from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from datetime import datetime, timedelta
 from . import schemas, database, models
+from .config import settings
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
 #SECRET_KEY
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+SECRET_KEY = settings.secret_key
 
 #Algorithm
-ALGORITHM = "HS256"
+ALGORITHM = settings.algorithm
 
 #EXPtime
-ACCESS_TOKEN_EXPIRE_MINUTES = 240
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
 
 def create_access_token(data: dict):
     to_encode = data.copy()
@@ -23,7 +24,7 @@ def create_access_token(data: dict):
     to_encode.update({"exp": expire})
 
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    
+
     return encoded_jwt
 
 def verify_access_token(token: str, credentials_exception):
