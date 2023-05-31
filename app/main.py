8fd58b3414f27840c536cd97de6c7335a90f0a55
HarrisonKim <wootak95@gmail.com>
2023-05-31 18:12:35 +0900

    remove unused function

--- app/main.py
+++ app/main.py
@@ -7,13 +7,9 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-my_posts = [{"title:": "title", "content": "content","id": 1}]
-
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
-
-
 
 @app.get("/")
 def root():
