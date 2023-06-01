b3393515efd262670fe1f4ef24f29d3d1d565eb0
HarrisonKim <wootak95@gmail.com>
2023-06-01 12:47:49 +0900

    fix table relationship issue(get all post owner va lue)

--- app/routers/post.py
+++ app/routers/post.py
@@ -31,10 +31,13 @@ def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends
         
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    post = db.query(models.Post).filter(models.Post.id==id)
-    if post.first() == None:
+    post_query = db.query(models.Post).filter(models.Post.id==id)
+    post = post_query.first()
+    if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
-    post.delete(synchronize_session=False)
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not Authorized")
+    post_query.delete(synchronize_session=False)
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
@@ -44,6 +47,8 @@ def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends
     post = post_query.first()
     if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    if post.owner_id != oauth2.current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not Authorized")
     post_query.update(updated_post.dict(), synchronize_session=False)
     db.commit()
     return post_query.first()
