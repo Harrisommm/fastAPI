37428935ff5f9150bf6fabb711523181599d1678
HarrisonKim <wootak95@gmail.com>
2023-06-07 13:35:12 +0900

    migrate alembic 	modified:   app/main.py

--- app/main.py
+++ app/main.py
@@ -2,9 +2,9 @@ from fastapi import FastAPI
 from . import models
 from .database import engine
 from .routers import post, user, auth, vote
-from .config import settings
 
-models.Base.metadata.create_all(bind=engine)
+#sqlalchemy table
+#models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
